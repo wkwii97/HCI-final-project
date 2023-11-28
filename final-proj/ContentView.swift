418a81cac053d51f0808fb0511d7cc63b88fd9dc
Warren K. Watson II <wwatson@ithaca.edu>
2023-11-28 11:26:49 -0500

    Initial Commit

---
+++ final-proj/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  final-proj
+//
+//  Created by Warren K. Watson II on 11/28/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ final-proj/final_projApp.swift
@@ -0,0 +1,17 @@
+//
+//  final_projApp.swift
+//  final-proj
+//
+//  Created by Warren K. Watson II on 11/28/23.
+//
+
+import SwiftUI
+
+@main
+struct final_projApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
