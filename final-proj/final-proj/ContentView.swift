94a9b38b46b5b44cc4c157ed52b617a4e8edc224
Warren K. Watson II <wwatson@ithaca.edu>
2023-11-28 12:02:59 -0500

    dummy commit

---
+++ final-proj/final-proj/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  final-proj
+//
+//  Created by Warren K. Watson II on 11/28/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("HCI Proj 1!!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
